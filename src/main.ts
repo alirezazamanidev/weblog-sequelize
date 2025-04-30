23c6023781bb9bcfe11a1f837f13378cda011ec5
Alireza Zamani <alirezazamanidev80@gmail.com>
2025-04-30 17:42:49 +0330

    Update dependencies and configure global API prefix with Swagger documentation path

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+
+  app.setGlobalPrefix('api')
+
   await app.listen(process.env.APP_PORT ?? 3000);
+  console.log(`Server running at http://localhost:${process.env.APP_PORT}/api`);
+  console.log(`swagger running at http://localhost:${process.env.APP_PORT}/doc`);
+  
 }
 bootstrap();
